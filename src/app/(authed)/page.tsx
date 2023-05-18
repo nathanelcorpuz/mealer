c205137670c3e76f002dca50f3368b43e08dcd00
natdlc <nathanelcorpuz@gmail.com>
2023-05-18 18:08:28 +0800

    remove sleep func

--- src/app/(authed)/page.tsx
+++ src/app/(authed)/page.tsx
@@ -1,10 +1,7 @@
 import PageWrapper from "@/components/PageWrapper";
 import Home from "./Home";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export default async function HomePage() {
-	await sleep(2000);
 	return (
 		<PageWrapper>
 			<Home />
